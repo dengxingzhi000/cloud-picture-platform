fcfc3410c1a78319ac9e6583580d48c11edd672d
awdev <developer@auditoryworks.tech>
2026-03-02 15:25:36 +0800

    chore(project): 初始化 Spring Boot 项目骨架

---
+++ src/main/java/com/cn/cloudpictureplatform/CloudPicturePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.cn.cloudpictureplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudPicturePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudPicturePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cn/cloudpictureplatform/CloudPicturePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cn.cloudpictureplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudPicturePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
